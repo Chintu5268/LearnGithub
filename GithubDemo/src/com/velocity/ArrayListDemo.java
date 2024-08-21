7cd74362feb5dee09245c4c05bf2c2e04defb2e4
Sandesh Sarade <Saradesandesh@gmail.com>
2024-08-21 19:41:38 +0530

    Arraylist code for name

---
+++ GithubDemo/src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		List<String> lis = new ArrayList<>();
+		lis.add("Sandesh");
+		lis.add("Piyush");
+		lis.add("Akash");
+		lis.add("Shivam");
+		lis.add("Rahul");
+		System.out.println(lis);
+
+	}
+
+}
