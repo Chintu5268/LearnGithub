7d6810d89c2ff1e921a933ae654c79c5beb69b9a
Sandesh Sarade <Saradesandesh@gmail.com>
2024-08-21 20:11:57 +0530

    Arraylist for integer code for name adding statement

--- GithubDemo/src/com/velocity/ArrylistDemo2.java
+++ GithubDemo/src/com/velocity/ArrylistDemo2.java
@@ -1,5 +1,16 @@
 package com.velocity;
 
-public class ArrylistDemo2 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ArrylistDemo2 {
+	public static void main(String[] args) {
+		List<Integer> lmIntegers = new ArrayList<>();
+		lmIntegers.add(25);
+		lmIntegers.add(85);
+		lmIntegers.add(95);
+		lmIntegers.add(36);
+		System.out.println(lmIntegers);
+		System.out.println("some code is genereted..."+lmIntegers);
+	}
 }
